205c0b6395aa9657d2abdd322b1d1a7ca2c848ea
lmachens <leon@machens.koeln>
2021-10-26 10:37:52 +0200

    Prepare users routes

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,17 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/api/users/:name', (_request, response) => {
+  response.send('Leon');
+});
+
+app.get('/api/users', (_request, response) => {
+  const users = ['Manuel', 'Leon', 'Anke'];
+  response.send(users);
+});
+
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World 🐱‍👤!');
 });
 
 app.listen(port, () => {
