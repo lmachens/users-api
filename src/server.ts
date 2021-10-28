0eae1d359c3132655e3878e80c23b21f3918ed3a
phlgr <philipp@gartz.dev>
2021-10-28 11:36:46 +0200

    ✨ Load MONGODB_URI from .env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { connectDatabase } from './utils/database';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('No MONGODB_URI provided');
+}
 
 const app = express();
 const port = 3000;
@@ -115,7 +122,7 @@ app.get('/', (_req, res) => {
   res.send('Hello World 🐱‍👤!');
 });
 
-connectDatabase('geheime mongodb url').then(() =>
+connectDatabase(process.env.MONGODB_URI).then(() =>
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   })
